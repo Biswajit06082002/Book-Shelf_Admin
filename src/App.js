d09616f117aea5b258c602044fadaa45479c2014
Biswajit <biswajitr022@gmail.com>
2023-07-27 18:29:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Signin from "./Signin";
+import { useState } from "react";
+import Confirmation from "./Confirmation";
+import Main from "./Main";
+import Products from "./Products";
+import ProductDetails from "./ProductDetails";
+import AddCategories from "./AddCategories";
+import PurchaseLog from "./PurchaseLog";
+import User from "./User";
+
+
+
+
+
 
 function App() {
+  const [confo, setConfo] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState("Academics");
+  const [product, setProduct] = useState("haha");
+
+  function setProductDetails(data){
+    setProduct(data);
+  }
+  function selectCategory(data){
+    setSelectedCategory(data);
+   }
+
+  function setConfirm(data){
+    setConfo(data);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Signin setConfirm={setConfirm}   />} />
+          <Route path="confirmation" element={<Confirmation confo = {confo}  />} />
+          <Route path="main" element={<Main selectCategory={selectCategory} setProductDetails={setProductDetails}/>} />
+          <Route path="products" element={<Products category = {selectedCategory} setProductDetails={setProductDetails}/>} />
+          <Route path="productdetails" element={<ProductDetails product={product}/>} />
+          <Route path="addcategories" element={<AddCategories product={product} selectCategory={selectCategory}/>} />
+          <Route path="purchaselog" element={<PurchaseLog setProductDetails={setProductDetails}/>} />
+          <Route path="manageuser" element={<User/>} />
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
